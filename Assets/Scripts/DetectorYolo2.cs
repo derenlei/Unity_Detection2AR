890ac36603c96f0643f72cf54ddf8100e40814c3
ROBYER1 <robyer1@googlemail.com>
2021-01-18 14:09:35 +0000

    Added platform and rendering system checks to see if the platform allows CPU/GPU Currently IOS has full GPU support with Metal API on IOS11 for all support ARKit Devices However, Android Vulka has GPU support but ARCore currently does not support Vulkan IOS:GPU support is enabled if we are using metal if not fall back to CPU Android:GPU support will be enabled if the device is running Vulkan, if not fall back to CPU

--- Assets/Scripts/DetectorYolo2.cs
+++ Assets/Scripts/DetectorYolo2.cs
@@ -58,9 +58,52 @@ public class DetectorYolo2 : MonoBehaviour, Detector
             .Where(s => !String.IsNullOrEmpty(s)).ToArray();
         var model = ModelLoader.Load(this.modelFile);
         // https://docs.unity3d.com/Packages/com.unity.barracuda@1.0/manual/Worker.html
-        // var workerType = WorkerFactory.Type.ComputePrecompiled; // GPU
-        var workerType = WorkerFactory.Type.CSharpBurst;  // CPU
-        this.worker = WorkerFactory.CreateWorker(workerType, model);
+        //These checks all check for GPU before CPU as GPU is preferred if the platform + rendering pipeline support it
+
+#if UNITY_IOS //Only IOS
+        Debug.Log("Graphics API: " + SystemInfo.graphicsDeviceType);
+        if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Metal)
+        {
+            //IOS 11 needed for ARKit, IOS 11 has Metal support only, therefore GPU can run
+            var workerType = WorkerFactory.Type.ComputePrecompiled; // GPU
+            this.worker = WorkerFactory.CreateWorker(workerType, model);
+        }
+        else
+        {
+            //If Metal support is dropped for some reason, fall back to CPU
+            var workerType = WorkerFactory.Type.CSharpBurst;  // CPU
+            this.worker = WorkerFactory.CreateWorker(workerType, model);
+        }
+
+#elif UNITY_ANDROID //Only Android
+        Debug.Log("Graphics API: " + SystemInfo.graphicsDeviceType);
+        if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Vulkan)
+        {
+            //Vulkan on Android supports GPU
+            //However, ARCore does not currently support Vulkan, when it does, this line will work
+            var workerType = WorkerFactory.Type.ComputePrecompiled; // GPU
+            this.worker = WorkerFactory.CreateWorker(workerType, model);
+        }
+        else
+        {
+            //If not vulkan, fall back to CPU
+            var workerType = WorkerFactory.Type.CSharpBurst;  // CPU
+            this.worker = WorkerFactory.CreateWorker(workerType, model);
+        }
+
+#elif UNITY_WEBGL //Only WebGL
+        Debug.Log("Graphics API: " + SystemInfo.graphicsDeviceType);
+     //WebGL only supports CPU
+    var workerType = WorkerFactory.Type.CSharpBurst;  // CPU
+    this.worker = WorkerFactory.CreateWorker(workerType, model);
+
+#else //Any other platform
+        Debug.Log("Graphics API: " + SystemInfo.graphicsDeviceType);
+    // https://docs.unity3d.com/Packages/com.unity.barracuda@1.0/manual/SupportedPlatforms.html
+    //var workerType = WorkerFactory.Type.CSharpBurst;  // CPU
+      var workerType = WorkerFactory.Type.ComputePrecompiled; // GPU
+      this.worker = WorkerFactory.CreateWorker(workerType, model);
+#endif
     }
 
 
